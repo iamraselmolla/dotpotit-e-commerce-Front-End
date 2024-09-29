5d68dbb00396c4ab58793a4aff7f2755b1a7164a
iamraselmolla <raselmolla6336@gmail.com>
2024-09-29 19:00:08 +0600

    added top categories

--- src/componenet/page/home/Home2/Home2.js
+++ src/componenet/page/home/Home2/Home2.js
@@ -3,13 +3,15 @@ import Hero from '../Hero';
 import Categories from '../Categories';
 import Products from '../../../shared/Product';
 import FeaturedBrands from '../FeaturesBand';
+import TopCategories from '../TopCategories';
 
 function Home2() {
     return (
         <div>
             <Hero />
-            <div className="grid md:grid-cols-2 grid-cols-1">
+            <div className="container mx-auto gap-4 p-4 grid md:grid-cols-2 grid-cols-1">
                 <FeaturedBrands />
+                <TopCategories />
             </div>
             < Categories />
             <Products />
--- src/componenet/page/home/TopCategories.js
+++ src/componenet/page/home/TopCategories.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 const TopCategories = () => {
     return (
-        <div>
-
-        </div>
+        <div className="bg-white rounded-lg mx-auto p-4 mt-2" >
+            < div className="flex justify-between items-center mb-4" >
+                <h2 className="text-2xl uppercase font-bold">top categories</h2>
+                <a href="/" className="text-blue-600 hover:underline">View All</a>
+            </div >
+            <div className="grid grid-cols-2 md:grid-cols-5 gap-4">
+                {Array.from({ length: 4 }).map((_, index) => (
+                    <div key={index} className="flex items-center justify-center p-4">
+                        <img src={`images/categories/cat (${index + 1}).png`} alt={`Brand ${index + 1}`} className="max-w-full object-contain" />
+                    </div>
+                ))}
+            </div>
+        </div >
     );
 };
 
